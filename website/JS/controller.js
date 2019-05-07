95c0920d091e369fa864209f34a00f31f613e649
Lisa Assignment2 <lisa.jacobsson@live.se>
2019-05-07 12:39:42 +0200

    cookies fixed

--- website/JS/controller.js
+++ website/JS/controller.js
@@ -1,159 +1,142 @@
 
 var app = angular
-.module("angularApp", ['ngRoute', 'ngCookies'])
+    .module("angularApp", ['ngRoute', 'ngCookies'])
 
-app.config(['$routeProvider', function($routeProvider){
+app.run(function ($rootScope, $cookies) {
+
+
+    var storedCart = $cookies.getObject('savecookies');
+
+    var expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() + 1);
+
+    if (storedCart) {
+        $rootScope.cart = storedCart;
+    } else {
+        $rootScope.cart = [];
+    }
+
+
+
+})
+
+
+
+app.config(['$routeProvider', function ($routeProvider) {
 
     $routeProvider
-    .when('/', {
-        templateUrl: 'view/card.html',
-        controller: 'productController'
-    })
-    .when('/card', {
-        templateUrl: 'view/card.html', 
-        controller: 'productController'
-    
-    })
-    .when('/list', {
-        templateUrl: 'view/list.html',
-        controller: 'productController'
-    })
-    .when('/login', {
-        templateUrl: 'view/login.html',
-        controller: 'userController'
-    })
-    .otherwise({
-        redirectionTo: '/'
-    })
- 
+        .when('/', {
+            templateUrl: 'view/card.html'
+        })
+        .when('/card', {
+            templateUrl: 'view/card.html'
+        })
+        .when('/list', {
+            templateUrl: 'view/list.html'
+        })
+        .otherwise({
+            redirectionTo: '/'
+        })
+
 }]);
 
 
+app.controller("productController", function ($rootScope, $http, $cookies) {
 
+    $rootScope.cart = [];
 
-
-
-
-app.controller("userController", function($rootScope, $http) {
-
-});
-
-app.controller("productController", function($rootScope, $http) {
     
+    if ($cookies.getObject("shoppingcart") !== null) {
+        $rootScope.cart = $cookies.getObject("shoppingcart");
+        tempcart = $cookies.getObject("shoppingcart");
+    }
+    var tempcart = [];
+
+
+
     const starsTotal = 5;
-    
-    var getProductId = function(products, id) {
-        return _.find(products, function(product) {
+
+    $http.get('http://localhost:5000/api/products')
+        .then(function (res) {
+            $rootScope.products = res.data;
+        });
+
+    var getProductId = function (products, id) {
+        return _.find(products, function (product) {
             return product.id === id
         });
     };
 
 
-    $http.get('http://localhost:5000/api/products')
-    .then(function(res) {
-        $rootScope.products = res.data;   
-    });
-   
-    $rootScope.rating = function(input) {
+    $rootScope.rating = function (input) {
         return `${Math.round(((input / starsTotal) * 100) / 10) * 10}%`;
-    } 
-  
-    $rootScope.cart = [];
-    $rootScope.emptycart = ""; 
+    }
 
-    $rootScope.addItem = function(product) {
+    $rootScope.emptycart = "Your cart is empty maddafakka";
+
+
+
+    var cookieExp = new Date();
+    cookieExp.setDate(cookieExp.getDate() + 7);
+
+    $rootScope.addItem = function (product) {
         var found = getProductId($rootScope.cart, product.id);
 
-        if(found) {
+
+        if (found) {
             found.quantity += product.quantity;
         }
         else {
-            $rootScope.cart.push(angular.copy(product));
+            tempcart.push(angular.copy(product));
+            $rootScope.cart = tempcart;
         }
+
+        $cookies.putObject("shoppingcart", $rootScope.cart, { expires: cookieExp });
+
     }
 
-    $rootScope.removeItem = function(product) {
+    $rootScope.removeItem = function (product) {
         var index = $rootScope.cart.indexOf(product);
         $rootScope.cart.splice(index, 1);
+        $cookies.putObject("shoppingcart", $rootScope.cart, { expires: cookieExp });
     }
 
-    $rootScope.getProductCost = function(product) {
+    $rootScope.getProductCost = function (product) {
         return product.quantity * product.price;
     }
 
-    $rootScope.getTotal = function() {
-        var total = _.reduce($rootScope.cart, function(sum, product) {
+    $rootScope.getProductQuantity = function (product) {
+        return product.quantity;
+    }
+
+    $rootScope.getTotal = function () {
+        var total = _.reduce($rootScope.cart, function (sum, product) {
             return sum + $rootScope.getProductCost(product);
         }, 0);
 
-        if($rootScope.cart.length === 0) {
-            $rootScope.emptycart = "Your cart is empty."
-        }
-        else {
-            $rootScope.emptycart = ""
-        }   
         return total;
-
-        $scope.addItem = function(product) {
-            var found = getProductId($scope.cart, product.id);
-        
-            if(found) {
-                found.quantity += product.quantity;
-            }
-            else {
-                $scope.cart.push(angular.copy(product));
-            }
-        }
-        $scope.removeItem = function(product) {
-            var index = $scope.cart.indexOf(product);
-            $scope.cart.splice(index, 1);
-        }
-        
-        $scope.getProductCost = function(product) {
-            return product.quantity * product.price;
-        }
-        
-        $scope.getTotal = function() {
-            var total = _.reduce($scope.cart, function(sum, product) {
-                return sum + $scope.getProductCost(product);
-            }, 0);
-        
-            if($scope.cart.length === 0) {
-                $scope.emptycart = "Your cart is empty."
-            }
-            else {
-                $scope.emptycart = ""
-            }   
-            return total;
-        }
     }
 
+    $rootScope.getQuantity = function () {
+        var quantity = _.reduce($rootScope.cart, function (sum, product) {
+            return sum + $rootScope.getProductQuantity(product);
+        }, 0);
 
-    run.$inject = ["$rootScope", "$location", "$cookies", "$http"];
-    function run($rootScope, $location, $cookies, $http) {
-
-        $rootScope.globals = $cookies.getObject("globals") || {};
-        if ($rootScope.globals.currentUser) {
-            $http.defaults.headers.common["Authorization"] = 'Basic ' +  $rootScope.globals.currentUser.authdata;
-            console.log($http.defaults.headers.common["Authorization"])
+        if (quantity === 0) {
+            $rootScope.badgeColor = "badge-secondary"
+        } else {
+            $rootScope.badgeColor = "badge-danger"
         }
 
-        $rootScope.$on("$locationChangeStart", function(event, next, current) {
-            var restrictedPage = $.inArray($location.path(), ["/login", "/register"]) === -1;
-            
-            var loggedIn = $rootScope.globals.currentUser;
-            if (restrictedPage && !loggedIn) {
-                $location.path("/login");
-            }
-        })
+        return quantity;
     }
-
 
 
 });
 
 
 
+
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
-  })
+})
--- website/services/dialog.service.js
+++ website/services/dialog.service.js
@@ -1,54 +1,51 @@
-(function () {
+angular
+    .module("demoApp")
+    .factory("dialogService", dialogService)
 
-    angular
-        .module("demoApp")
-        .factory("dialogService", dialogService)
+dialogService.$inject = ["$rootScope"];
+function dialogService($rootScope) {
 
-    dialogService.$inject = ["$rootScope"];
-    function dialogService($rootScope) {
+    var service = {};
 
-        var service = {};
+    function Success(message, keepAfterLocationChange) {
+        $rootScope.dialog = {
+            message: message,
+            type: 'success',
+            keepAfterLocationChange: keepAfterLocationChange
+        }
+    };
 
-        function Success(message, keepAfterLocationChange) {
-            $rootScope.dialog = {
-                message: message,
-                type: 'success',
-                keepAfterLocationChange: keepAfterLocationChange
-            }
-        };
+    function Error(message, keepAfterLocationChange) {
+        $rootScope.dialog = {
+            message: message,
+            type: 'error',
+            keepAfterLocationChange: keepAfterLocationChange
+        }
+    };
 
-        function Error(message, keepAfterLocationChange) {
-            $rootScope.dialog = {
-                message: message,
-                type: 'error',
-                keepAfterLocationChange: keepAfterLocationChange
-            }
-        };
+    function initService() {
 
-        function initService() {
-            
-            function clearDialogMessage() {
-                var dialog =  $rootScope.dialog;
-                if (dialog) {
-                    if (!dialog.keepAfterLocationChange) {
-                        delete $rootScope.dialog;
-                    } else {
-                        dialog.keepAfterLocationChange = false;
-                    }
+        function clearDialogMessage() {
+            var dialog = $rootScope.dialog;
+            if (dialog) {
+                if (!dialog.keepAfterLocationChange) {
+                    delete $rootScope.dialog;
+                } else {
+                    dialog.keepAfterLocationChange = false;
                 }
             }
-
-            
-            $rootScope.$on("$locationChangeStart", function() {
-                clearDialogMessage();
-            })
         }
 
 
-        service.Success = Success;
-        service.Error = Error;
-
-        initService();
-        return service;
+        $rootScope.$on("$locationChangeStart", function () {
+            clearDialogMessage();
+        })
     }
-})();
+
+
+    service.Success = Success;
+    service.Error = Error;
+
+    initService();
+    return service;
+}
